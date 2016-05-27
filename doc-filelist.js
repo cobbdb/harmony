02681a3d6bb10e8480ce2abc009c522560e1c0d1
Dan <cobbdb@gmail.com>
2016-05-26 23:42:24 -0600

    .

--- doc-filelist.js
+++ doc-filelist.js
@@ -1,1 +1,1 @@
-var tree={"dirs":{"actions":{"files":["disable.js","enable.js","refresh.js"]}},"files":["adslot.js","breakpoint-watcher.js","event-handler.js","group-set.js","harmony.js","log.js","screen.js","slot-set.js","util.js"]};
+var tree={"dirs":{"actions":{"files":["define-slot.js","disable.js","enable.js","hide.js","load.js","refresh.js","show.js"]}},"files":["adslot.js","breakpoint-watcher.js","event-handler.js","group-set.js","harmony.js","log.js","screen.js","slot-set.js","util.js"]};
